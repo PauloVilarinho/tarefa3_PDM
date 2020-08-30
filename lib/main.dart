4fcf59eab42482dceadf9038648caa999597271f
Paulo Vilarinho <paulotarsobranco@hotmail.com>
2020-08-30 14:40:12 -0300

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,64 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.grey,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Text('Like'),
+          backgroundColor: Colors.grey,
         ),
-        body: DicePage(),
+        body: LikePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class LikePage extends StatefulWidget {
+  @override
+  _LikePageState createState() => _LikePageState();
+}
+
+class _LikePageState extends State<LikePage> {
+  bool like = false;
+  bool dislike = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: like ? 
+          Image.asset('images/happy_sora.jpg') 
+          : dislike ?  Image.asset('images/sad_sora.png') 
+          : Image.asset('images/normal_sora.png'),
+        ),
+        Row(
+          children: <Widget>[ Expanded(child:FlatButton.icon(
+              textColor: Colors.white,
+              icon: Icon(Icons.thumb_up,
+              color: like? Colors.green : Colors.white),
+              label: Text('Like',
+              style: TextStyle(color: like? Colors.green : Colors.white)),
+              onPressed: () {setState(() {
+                like = !like;
+                dislike = false;
+              });},
+                     ),
+            ),
+            Expanded(child:FlatButton.icon(
+              textColor: Colors.white,
+              icon: Icon(Icons.thumb_down,
+              color: dislike? Colors.red : Colors.white),
+              label: Text('Dislike',
+              style: TextStyle(color: dislike? Colors.red : Colors.white)),
+              onPressed: () {setState(() {
+                like = false;
+                dislike = !dislike;
+              });},
+                     ),
+            ),
+          ],
+          ),
+      ]
+      ,);
   }
 }
